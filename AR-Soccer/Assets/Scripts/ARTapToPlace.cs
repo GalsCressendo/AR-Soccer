3df7a6f032d840266ad6f078120d1dfb6eb8ce2a
Galuh Kirana Miftahuljannah <galuhkmj@gmail.com>
2022-12-07 18:51:20 +0700

    pinch to zoom

--- AR-Soccer/Assets/Scripts/ARTapToPlace.cs
+++ AR-Soccer/Assets/Scripts/ARTapToPlace.cs
@@ -14,44 +14,68 @@ public class ARTapToPlace : MonoBehaviour
     Vector2 touchPosition;
     static List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
+    //PinchToZoom
+    Vector2 first_touch;
+    Vector2 second_touch;
+    float distance_current;
+    float distance_previous;
+    bool first_pinch = true;
+
     private void Awake()
     {
         arRaycastManager = GetComponent<ARRaycastManager>();
         arPlaneManager = GetComponent<ARPlaneManager>();
     }
 
-    bool IsTouching(out Vector2 touchPosition)
+    private void Update()
     {
         if(Input.touchCount > 0)
         {
+
             touchPosition = Input.GetTouch(0).position;
-            return true;
+
+            if (arRaycastManager.Raycast(touchPosition, hits, TrackableType.Planes))
+            {
+                var hitPose = hits[0].pose;
+
+                if (spawnObject == null)
+                {
+                    spawnObject = Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
+                    foreach (var plane in arPlaneManager.trackables)
+                    {
+                        plane.gameObject.SetActive(false);
+                    }
+
+                    arPlaneManager.enabled = false;
+
+                }
+            }
         }
 
-        touchPosition = default;
-        return false;
-    }
+       
 
-    private void Update()
-    {
-        if (!IsTouching(out Vector2 touchPosition))
-            return;
-
-        if(arRaycastManager.Raycast(touchPosition,hits, TrackableType.Planes))
+        if(Input.touchCount>1 && spawnObject != null)
         {
-            var hitPose = hits[0].pose;
+            first_touch = Input.GetTouch(0).position;
+            second_touch = Input.GetTouch(1).position;
 
-            if(spawnObject == null)
+            distance_current = second_touch.magnitude - first_touch.magnitude;
+            if (first_pinch)
             {
-                spawnObject = Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
-                foreach (var plane in arPlaneManager.trackables)
-                {
-                    plane.gameObject.SetActive(false);
-                }
-
-                arPlaneManager.enabled = false;
-
+                distance_previous = distance_current;
+                first_pinch = false;
             }
+
+            if (distance_current != distance_previous)
+            {
+                Vector3 scale_value = spawnObject.transform.localScale * (distance_current / distance_previous);
+                spawnObject.transform.localScale = scale_value;
+                distance_previous = distance_current;
+            }
+        }
+        else
+        {
+            first_pinch = true;
         }
     }
 
