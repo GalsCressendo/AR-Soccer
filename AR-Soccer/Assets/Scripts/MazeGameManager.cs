2f03f86e31ac386b487baa50984896f6a10efc51
Galuh Kirana Miftahuljannah <galuhkmj@gmail.com>
2022-12-12 19:22:08 +0700

    generate navmesh

--- AR-Soccer/Assets/Scripts/MazeGameManager.cs
+++ AR-Soccer/Assets/Scripts/MazeGameManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MazeGameManager : MonoBehaviour
 {
+    public NavMeshSurface surface;
     [SerializeField] List<GameObject> mazes;
     [SerializeField] GameObject field;
 
@@ -12,5 +14,6 @@ public class MazeGameManager : MonoBehaviour
         int rand = Random.Range(0, mazes.Count);
         GameObject maze = Instantiate(mazes[rand], mazes[rand].transform.position, mazes[rand].transform.rotation);
         maze.transform.SetParent(field.transform, false);
+        surface.BuildNavMesh();
     }
 }
