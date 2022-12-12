33289f34410b10ac1bbd754c54edcc4d4e5d7c57
Galuh Kirana Miftahuljannah <galuhkmj@gmail.com>
2022-12-12 19:05:53 +0700

    generate maze

---
+++ AR-Soccer/Assets/Scripts/MazeGameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGameManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> mazes;
+    [SerializeField] GameObject field;
+
+    private void Awake()
+    {
+        int rand = Random.Range(0, mazes.Count);
+        GameObject maze = Instantiate(mazes[rand], mazes[rand].transform.position, mazes[rand].transform.rotation);
+        maze.transform.SetParent(field.transform, false);
+    }
+}
