d3f334849ee4f5c3d6dfac1b897ad6675f89d9c0
Galuh Kirana Miftahuljannah <galuhkmj@gmail.com>
2022-12-12 20:31:26 +0700

    turn on indicator and animation

--- AR-Soccer/Assets/Scripts/PlayerMazeController.cs
+++ AR-Soccer/Assets/Scripts/PlayerMazeController.cs
@@ -8,24 +8,29 @@ public class PlayerMazeController : MonoBehaviour
     NavMeshAgent agent;
     const string BALL_TAG = "Ball";
     const string GOAL_TARGET = "Goal_Red";
+    const string RUNNING_ANIM_PARAM = "isRunning";
     Transform ballTarget = null;
+    Animator animator;
 
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if (GameObject.FindGameObjectWithTag(BALL_TAG))
+        if (GameObject.FindGameObjectWithTag(BALL_TAG) && !GameObject.FindGameObjectWithTag(BALL_TAG).GetComponent<Ball>().isAttached)
         {
+            animator.SetBool(RUNNING_ANIM_PARAM, true);
             ballTarget = GameObject.FindGameObjectWithTag(BALL_TAG).transform;
             if (!ballTarget.GetComponent<Ball>().isAttached)
             {
                 agent.SetDestination(ballTarget.position);
                 if(Vector3.Distance(transform.position, ballTarget.position) < 0.001f)
                 {
+                    ballTarget.GetComponent<Ball>().isAttached = true;
                     Debug.Log("Have Arrived");
                 }
             }
